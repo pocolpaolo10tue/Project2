3fc95c1320e19f698ce5984e67f606fe7eeb4825
lucas <L.w.t.lahaije@student.tue.nl>
2022-03-22 13:26:24 +0100

    Added login fragment and worked on the preferences (settings)

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,23 +1,11 @@
 package com.example.project2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -33,45 +21,44 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        //BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .add(R.id.bottomNavigationView, mapsFragment)
                 .commit();
 
-        bottomNav.setOnItemSelectedListener(navListener);
+        //.setOnItemSelectedListener(navListener);
 
     }
 
-    private  NavigationBarView.OnItemSelectedListener navListener = new BottomNavigationView.OnItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            // By using switch we can easily get
-            // the selected fragment
-            // by using there id.
-            Fragment selectedFragment = null;
-            switch (item.getItemId()) {
-                case R.id.maps_Fragment:
-                    selectedFragment = new MapsFragment();
-                    break;
-                case R.id.book_Fragment:
-                    selectedFragment = new BookFragment();
-                    break;
-                case R.id.event_Fragment:
-                    selectedFragment = new EventFragment();
-                    break;
-                case R.id.settings_Fragment:
-                    selectedFragment = new SettingsFragment();
-                    break;
+    private final BottomNavigationView.OnItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.maps_Fragment) {
+                selectedFragment = new MapsFragment();
             }
-            //replace fragment
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.bottomNavigationView, selectedFragment)
-                    .commit();
-            return true;
-        }
+            else if (item.getItemId() == R.id.book_Fragment) {
+                selectedFragment = new BookFragment();
+            }
+            else if (item.getItemId() == R.id.event_Fragment) {
+                selectedFragment = new EventFragment();
+            }
+            else if (item.getItemId() == R.id.settings_Fragment) {
+                selectedFragment = new SettingsFragment();
+            }
+            else if (item.getItemId() == R.id.login_Fragment) {
+                selectedFragment = new SettingsFragment();
+            }
+        //replace fragment
+        assert selectedFragment != null;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.bottomNavigationView, selectedFragment)
+                .commit();
+        return true;
     };
 
     @Override
